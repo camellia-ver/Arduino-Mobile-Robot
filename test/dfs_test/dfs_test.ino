cc0b3c97f9fccd041b1791bf72189c6028205db3
camellia-ver <57394293+camellia-ver@users.noreply.github.com>
2025-04-30 09:25:41 +0900

    Create dfs_test.ino

---
+++ test/dfs_test/dfs_test.ino
@@ -0,0 +1,96 @@
+#define GRID_SIZE 8
+
+uint8_t grid[GRID_SIZE][GRID_SIZE] = {
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 1, 1, 1, 0, 0, 0, 0},
+  {0, 0, 0, 1, 0, 0, 0, 0},
+  {0, 0, 0, 1, 0, 0, 0, 0},
+  {0, 0, 0, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+struct Point {
+  int x;
+  int y;
+};
+
+int directions[4][2] = {{-1,0},{1,0},{0,-1},{0,1}}; // 상,하,좌,우
+
+Point queue[GRID_SIZE * GRID_SIZE]; // 큐
+int front = 0, rear = 0; // 큐의 앞과 뒤
+
+bool visited[GRID_SIZE][GRID_SIZE]; // 방문 여부
+int dist[GRID_SIZE][GRID_SIZE]; // 거리 배열
+
+void enqueue(int x,int y){
+  queue[rear].x = x;
+  queue[rear].y = y;
+  rear = (rear + 1) % (GRID_SIZE * GRID_SIZE);
+}
+
+Point dequeue(){
+  Point p = queue[front];
+  front = (front + 1) % (GRID_SIZE * GRID_SIZE);
+  return p;
+}
+
+// BFS
+int bfs(Point start, Point goal){
+  // 초기화
+  memset(visited, false, sizeof(visited));
+  memset(dist, -1, sizeof(dist)); // -1 : 미방문
+  front = rear = 0;
+
+  enqueue(start.x, start.y);
+  visited[start.x][start.y];
+  dist[start.x][start.y];
+
+  while (front != rear){
+    Point current = dequeue();
+
+    // 목표 지점에 도달시 거리 반환
+    if (current.x == goal.x && current.y == goal.y){
+      return dist[current.x][current.y];
+    }
+
+    // 상, 하, 좌, 우로 이동
+    for (int i = 0; i < 4; i++){
+      int nx = current.x + directions[i][0];
+      int ny = current.y + directions[i][1];
+
+      // 유효 좌표 and 장애물 x and 방문 x
+      if (nx >= 0 && nx < GRID_SIZE && ny >= 0 && ny < GRID_SIZE && grid[nx][ny] == 0 && !visited[nx][ny]){
+        enqueue(nx,ny);
+        visited[nx][ny] = true;
+        dist[nx][ny] = dist[current.x][current.y] + 1;
+      }
+    }
+  }
+
+  return -1; // 경로가 없을 경우
+}
+
+void setup() {
+  Serial.begin(115200);
+  Point startPoint = {0,0};
+  Point goalPoint = {7,6};
+
+  Serial.print("목표 위치: ");
+    Serial.print(goalPoint.x); 
+    Serial.print(", ");
+    Serial.println(goalPoint.y);
+
+    int result = bfs(startPoint,goalPoint);
+
+    if (result != -1){
+      Serial.print("최단 경로의 길이: ");
+      Serial.println(result);
+    }else{
+      Serial.println("목표 지점에 도달할 수 없습니다.");
+    }
+}
+
+void loop() {
+}
